1dca55b3343a9a4d965ff2eb5cdaf291af1ac2c7
Moises B <mbaez091@gmail.com>
2023-04-06 19:16:52 -0400

    refactor

--- src/App.js
+++ src/App.js
@@ -13,184 +13,7 @@ import { authService } from "./authService";
 import { history } from "./history";
 import { RequireAuth } from "./RequireAuth";
 import { Users } from "./Users";
-import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
-import Toolbar from "@mui/material/Toolbar";
-import IconButton from "@mui/material/IconButton";
-import Typography from "@mui/material/Typography";
-import Menu from "@mui/material/Menu";
-import MenuIcon from "@mui/icons-material/Menu";
-import Container from "@mui/material/Container";
-import Avatar from "@mui/material/Avatar";
-import Button from "@mui/material/Button";
-import Tooltip from "@mui/material/Tooltip";
-import MenuItem from "@mui/material/MenuItem";
-
-function ResponsiveAppBar({ currentUser, logOut }) {
-  const [anchorElNav, setAnchorElNav] = React.useState(null);
-  const [anchorElUser, setAnchorElUser] = React.useState(null);
-
-  const handleOpenNavMenu = (event) => {
-    setAnchorElNav(event.currentTarget);
-  };
-  const handleOpenUserMenu = (event) => {
-    setAnchorElUser(event.currentTarget);
-  };
-
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
-  };
-
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
-  };
-
-  return (
-    <AppBar position="static">
-      <Container maxWidth="xl">
-        <Toolbar disableGutters>
-          <Typography
-            variant="h6"
-            noWrap
-            component="a"
-            href="/"
-            sx={{
-              mr: 2,
-              display: { xs: "none", md: "flex" },
-              fontFamily: "monospace",
-              fontWeight: 700,
-              letterSpacing: ".3rem",
-              color: "inherit",
-              textDecoration: "none",
-            }}
-          >
-            Users
-          </Typography>
-
-          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: "bottom",
-                horizontal: "left",
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "left",
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{
-                display: { xs: "block", md: "none" },
-              }}
-            >
-              <MenuItem onClick={handleCloseNavMenu}>
-                <Typography textAlign="center">List</Typography>
-              </MenuItem>
-            </Menu>
-          </Box>
-
-          <Typography
-            variant="h5"
-            noWrap
-            component="a"
-            href=""
-            sx={{
-              mr: 2,
-              display: { xs: "flex", md: "none" },
-              flexGrow: 1,
-              fontFamily: "monospace",
-              fontWeight: 700,
-              letterSpacing: ".3rem",
-              color: "inherit",
-              textDecoration: "none",
-            }}
-          >
-            Users
-          </Typography>
-
-          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
-            <Button
-              onClick={handleCloseNavMenu}
-              sx={{ my: 2, color: "white", display: "block" }}
-            >
-              List
-            </Button>
-          </Box>
-
-          <Box sx={{ flexGrow: 0 }}>
-            <Tooltip title="Open settings">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
-              </IconButton>
-            </Tooltip>
-            <Menu
-              sx={{ mt: "45px" }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: "top",
-                horizontal: "right",
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "right",
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-            >
-              {currentUser?.decodedToken && [
-                <MenuItem key="username" disabled={true}>
-                  <Typography variant="body2" textAlign="center">
-                    {currentUser.decodedToken.username}
-                  </Typography>
-                </MenuItem>,
-                <MenuItem key="name" disabled={true}>
-                  <Typography variant="body2" textAlign="center">
-                    {`${currentUser.decodedToken.firstname} ${currentUser.decodedToken.lastname}`}
-                  </Typography>
-                </MenuItem>,
-                <MenuItem key="email" disabled={true} divider={true}>
-                  <Typography variant="body2" textAlign="center">
-                    {currentUser.decodedToken.email}
-                  </Typography>
-                </MenuItem>,
-              ]}
-
-              {currentUser?.decodedToken && (
-                <MenuItem key="logout" onClick={logOut}>
-                  <Typography textAlign="center">Log out</Typography>
-                </MenuItem>
-              )}
-
-              {!currentUser?.decodedToken && (
-                <MenuItem
-                  key="logout"
-                  onClick={() => history.navigate("/login")}
-                >
-                  <Typography textAlign="center">Log in</Typography>
-                </MenuItem>
-              )}
-            </Menu>
-          </Box>
-        </Toolbar>
-      </Container>
-    </AppBar>
-  );
-}
+import { ResponsiveAppBar } from "./ResponsiveAppBar";
 
 function App() {
   // Init custom history object to allow navigation from
@@ -219,31 +42,29 @@ function App() {
     <>
       <ResponsiveAppBar currentUser={currentUser} logOut={logOut} />
 
-      <div>
-        <Routes>
-          <Route
-            path="/login"
-            element={<Login setCurrentUser={setCurrentUser} />}
-          />
-          <Route
-            path="/home"
-            element={
-              <RequireAuth>
-                <Home />
-              </RequireAuth>
-            }
-          />
-          <Route
-            path="/users"
-            element={
-              <RequireAuth>
-                <Users />
-              </RequireAuth>
-            }
-          />
-          <Route path="/" element={<Landing />} />
-        </Routes>
-      </div>
+      <Routes>
+        <Route
+          path="/login"
+          element={<Login setCurrentUser={setCurrentUser} />}
+        />
+        <Route
+          path="/home"
+          element={
+            <RequireAuth>
+              <Home />
+            </RequireAuth>
+          }
+        />
+        <Route
+          path="/users"
+          element={
+            <RequireAuth>
+              <Users />
+            </RequireAuth>
+          }
+        />
+        <Route path="/" element={<Landing />} />
+      </Routes>
     </>
   );
 }
---
+++ src/ResponsiveAppBar.js
@@ -0,0 +1,192 @@
+import React, { useState } from "react";
+import { history } from "./history";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import Tooltip from "@mui/material/Tooltip";
+import MenuItem from "@mui/material/MenuItem";
+import PersonIcon from "@mui/icons-material/Person";
+
+function ResponsiveAppBar({ currentUser, logOut }) {
+  const [anchorElNav, setAnchorElNav] = useState(null);
+  const [anchorElUser, setAnchorElUser] = useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
+  const closeMenuAndGoToUserList = () => {
+    history.navigate("/users");
+    handleCloseNavMenu();
+  };
+
+  return (
+    <AppBar position="static">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="/"
+            sx={{
+              mr: 2,
+              display: { xs: "none", md: "flex" },
+              fontFamily: "monospace",
+              fontWeight: 700,
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none",
+            }}
+          >
+            Users
+          </Typography>
+
+          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "left",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "left",
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: "block", md: "none" },
+              }}
+            >
+              <MenuItem onClick={closeMenuAndGoToUserList}>
+                <Typography textAlign="center">List</Typography>
+              </MenuItem>
+            </Menu>
+          </Box>
+
+          <Typography
+            variant="h5"
+            noWrap
+            component="a"
+            href=""
+            sx={{
+              mr: 2,
+              display: { xs: "flex", md: "none" },
+              flexGrow: 1,
+              fontFamily: "monospace",
+              fontWeight: 700,
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none",
+            }}
+          >
+            Users
+          </Typography>
+
+          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+            <Button
+              onClick={closeMenuAndGoToUserList}
+              sx={{ my: 2, color: "white", display: "block" }}
+            >
+              List
+            </Button>
+          </Box>
+
+          <Box sx={{ flexGrow: 0 }}>
+            <Tooltip title="Open settings">
+              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                <Avatar
+                  alt="Remy Sharp"
+                  src="/something.png"
+                  children={<PersonIcon />}
+                />
+              </IconButton>
+            </Tooltip>
+            <Menu
+              sx={{ mt: "45px" }}
+              id="menu-appbar"
+              anchorEl={anchorElUser}
+              anchorOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}
+            >
+              {currentUser?.decodedToken && [
+                <MenuItem key="username" disabled={true}>
+                  <Typography variant="body2" textAlign="center">
+                    {currentUser.decodedToken.username}
+                  </Typography>
+                </MenuItem>,
+                <MenuItem key="name" disabled={true}>
+                  <Typography variant="body2" textAlign="center">
+                    {`${currentUser.decodedToken.firstname} ${currentUser.decodedToken.lastname}`}
+                  </Typography>
+                </MenuItem>,
+                <MenuItem key="email" disabled={true} divider={true}>
+                  <Typography variant="body2" textAlign="center">
+                    {currentUser.decodedToken.email}
+                  </Typography>
+                </MenuItem>,
+              ]}
+
+              {currentUser?.decodedToken && (
+                <MenuItem key="logout" onClick={logOut}>
+                  <Typography textAlign="center">Log out</Typography>
+                </MenuItem>
+              )}
+
+              {!currentUser?.decodedToken && (
+                <MenuItem
+                  key="logout"
+                  onClick={() => history.navigate("/login")}
+                >
+                  <Typography textAlign="center">Log in</Typography>
+                </MenuItem>
+              )}
+            </Menu>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+}
+
+export { ResponsiveAppBar };
