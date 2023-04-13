23ed9a15ee138e30a58b5819ce5ca59a98a15f3c
Moises B <mbaez091@gmail.com>
2023-04-13 00:04:49 -0400

    loader for login

--- src/Login.js
+++ src/Login.js
@@ -12,11 +12,13 @@ import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import { authService } from "./authService";
+import { green } from "@mui/material/colors";
 
 import { useNavigate } from "react-router-dom";
 import { CircularProgress } from "@mui/material";
 
 import api from "./api";
+import { sleep } from "./utils";
 
 function Login({ setCurrentUser, setCurrentUserData }) {
   const navigate = useNavigate();
@@ -65,61 +67,71 @@ function Login({ setCurrentUser, setCurrentUserData }) {
   return (
     <Container component="main" maxWidth="xs">
       <CssBaseline />
-      {loading ? (
-        <CircularProgress />
-      ) : (
-        <Box
-          sx={{
-            marginTop: 8,
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-          }}
-        >
-          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Sign in
-          </Typography>
+      <Box
+        sx={{
+          marginTop: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
 
-          <Box
-            component="form"
-            onSubmit={handleSubmit}
-            noValidate
-            sx={{ mt: 1 }}
-          >
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="username"
-              label="Username"
-              name="username"
-              autoComplete="username"
-              autoFocus
-            />
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-            />
+        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="username"
+            label="Username"
+            name="username"
+            autoComplete="username"
+            autoFocus
+          />
+
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+            sx={{ mb: 3 }}
+          />
+
+          <Box sx={{ position: "relative" }}>
             <Button
               type="submit"
               fullWidth
               variant="contained"
-              sx={{ mt: 3, mb: 2 }}
+              // sx={buttonSx}
+              disabled={loading}
             >
-              Sign In
+              Sign in
             </Button>
+            {loading && (
+              <CircularProgress
+                size={24}
+                sx={{
+                  color: green[500],
+                  position: "absolute",
+                  top: "50%",
+                  left: "50%",
+                  marginTop: "-12px",
+                  marginLeft: "-12px",
+                }}
+              />
+            )}
           </Box>
         </Box>
-      )}
+      </Box>
     </Container>
   );
 }
--- src/utils.js
+++ src/utils.js
@@ -9,4 +9,10 @@ function getRandomIntInclusive(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
 }
 
-export { getCurrentMilliseconds, getRandomIntInclusive };
+function sleep(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
+
+export { getCurrentMilliseconds, getRandomIntInclusive, sleep };
